63be967a78c59ff7e0b6af8495bc0bd1a80852d5
Mohab Usama <mohab.usama@gmail.com>
2016-11-09 16:53:53 +0100

    Fix kube client & typo (#3)

--- k8s_log_watcher/kube.py
+++ k8s_log_watcher/kube.py
@@ -2,13 +2,17 @@ import pykube
 
 
 DEFAULT_SERVICE_ACC = '/var/run/secrets/kubernetes.io/serviceaccount'
+DEFAULT_NAMESPACE = 'default'
 
-config = pykube.KubeConfig.from_file(DEFAULT_SERVICE_ACC)
-kube_client = pykube.HTTPClient(config)
+
+def get_client():
+    config = pykube.KubeConfig.from_service_account(DEFAULT_SERVICE_ACC)
+    return pykube.HTTPClient(config)
 
 
 def get_pods() -> list:
-    return pykube.Pod.objects(kube_client).filter(namespace='dafault')
+    kube_client = get_client()
+    return pykube.Pod.objects(kube_client).filter(namespace=DEFAULT_NAMESPACE)
 
 
 def get_pod_labels(pods: list, pod_name: str) -> dict:
