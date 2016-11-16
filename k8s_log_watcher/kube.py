80225cbc69e9ebab872638e321e9ce42f8a8c077
Mohab Usama <mohab.usama@gmail.com>
2016-11-16 17:17:58 +0100

    Support multiple namespaces (#11)
    Fix #9

--- k8s_log_watcher/kube.py
+++ k8s_log_watcher/kube.py
@@ -7,7 +7,7 @@ import requests
 DEFAULT_SERVICE_ACC = '/var/run/secrets/kubernetes.io/serviceaccount'
 DEFAULT_NAMESPACE = 'default'
 
-PODS_URL = 'api/v1/namespaces/default/pods'
+PODS_URL = 'api/v1/namespaces/{}/pods'
 
 PAUSE_CONTAINER_PREFIX = 'gcr.io/google_containers/pause-'
 
@@ -17,7 +17,7 @@ def get_client():
     return pykube.HTTPClient(config)
 
 
-def get_pods(kube_url=None) -> list:
+def get_pods(kube_url=None, namespace=DEFAULT_NAMESPACE) -> list:
     """
     Return list of pods in cluster. If ``kube_url`` is not ``None`` then K8S service account config won't be used.
 
@@ -25,18 +25,21 @@ def get_pods(kube_url=None) -> list:
                      to proxy service instead of depending on serviceaccount config. Default is ``None``.
     :type kube_url: str
 
+    :param namespace: Desired namespace of the pods. Default namespace is ``default``.
+    :type namespace: str
+
     :return: List of pods.
     :rtype: list
     """
     if kube_url:
-        r = requests.get(urljoin(kube_url, PODS_URL))
+        r = requests.get(urljoin(kube_url, PODS_URL.format(namespace)))
 
         r.raise_for_status()
 
         return r.json().get('items', [])
 
     kube_client = get_client()
-    return pykube.Pod.objects(kube_client).filter(namespace=DEFAULT_NAMESPACE)
+    return pykube.Pod.objects(kube_client).filter(namespace=namespace)
 
 
 def get_pod_labels(pods: list, pod_name: str) -> dict:
